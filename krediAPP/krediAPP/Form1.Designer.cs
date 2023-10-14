5fa950f0409b47aba57e5d44edfcf0537fc00800
Yusuf Söyler <yusufsoyler5222@gmail.com>
2023-10-15 02:26:04 +0300

    v1
    ""

---
+++ krediAPP/krediAPP/Form1.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace krediAPP
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox1 = new TextBox();
+            dateTimePicker1 = new DateTimePicker();
+            label1 = new Label();
+            button1 = new Button();
+            dateTimePicker2 = new DateTimePicker();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            button2 = new Button();
+            comboBox1 = new ComboBox();
+            label6 = new Label();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.BackColor = Color.LightSkyBlue;
+            textBox1.Location = new Point(403, 43);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(105, 23);
+            textBox1.TabIndex = 0;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Location = new Point(29, 372);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(200, 23);
+            dateTimePicker1.TabIndex = 1;
+            dateTimePicker1.ValueChanged += dateTimePicker1_ValueChanged;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.FromArgb(255, 192, 128);
+            label1.Location = new Point(254, 423);
+            label1.Name = "label1";
+            label1.Size = new Size(51, 15);
+            label1.TabIndex = 2;
+            label1.Text = "kontrol1";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(235, 372);
+            button1.Name = "button1";
+            button1.Size = new Size(105, 39);
+            button1.TabIndex = 3;
+            button1.Text = "Vade Hesapla";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // dateTimePicker2
+            // 
+            dateTimePicker2.Location = new Point(29, 417);
+            dateTimePicker2.Name = "dateTimePicker2";
+            dateTimePicker2.Size = new Size(200, 23);
+            dateTimePicker2.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            textBox2.BackColor = Color.LightSkyBlue;
+            textBox2.Location = new Point(132, 105);
+            textBox2.Multiline = true;
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(121, 23);
+            textBox2.TabIndex = 5;
+            textBox2.TextChanged += textBox2_TextChanged;
+            // 
+            // textBox3
+            // 
+            textBox3.BackColor = Color.LightSkyBlue;
+            textBox3.Location = new Point(403, 108);
+            textBox3.Multiline = true;
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(105, 23);
+            textBox3.TabIndex = 6;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(30, 108);
+            label2.Name = "label2";
+            label2.Size = new Size(80, 15);
+            label2.TabIndex = 7;
+            label2.Text = "TAKSİT SAYISI:";
+            label2.Click += label2_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(313, 111);
+            label3.Name = "label3";
+            label3.Size = new Size(72, 15);
+            label3.TabIndex = 8;
+            label3.Text = "FAİZ ORANI:";
+            label3.Click += label3_Click;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(321, 46);
+            label4.Name = "label4";
+            label4.Size = new Size(64, 15);
+            label4.TabIndex = 9;
+            label4.Text = "ANAPARA:";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.BackColor = Color.FromArgb(255, 192, 128);
+            label5.Location = new Point(461, 195);
+            label5.Name = "label5";
+            label5.Size = new Size(47, 15);
+            label5.TabIndex = 10;
+            label5.Text = "SONUÇ";
+            label5.Click += label5_Click;
+            // 
+            // button2
+            // 
+            button2.BackColor = SystemColors.InactiveCaption;
+            button2.Location = new Point(235, 173);
+            button2.Name = "button2";
+            button2.Size = new Size(105, 37);
+            button2.TabIndex = 11;
+            button2.Text = "HESAPLA";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
+            // 
+            // comboBox1
+            // 
+            comboBox1.BackColor = Color.LightSkyBlue;
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Items.AddRange(new object[] { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "36", "48", "72", "96" });
+            comboBox1.Location = new Point(132, 40);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(121, 23);
+            comboBox1.TabIndex = 12;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(15, 43);
+            label6.Name = "label6";
+            label6.Size = new Size(99, 15);
+            label6.TabIndex = 13;
+            label6.Text = "VADE SÜRESİ(AY):";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.PaleGreen;
+            ClientSize = new Size(643, 302);
+            Controls.Add(label6);
+            Controls.Add(comboBox1);
+            Controls.Add(button2);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(dateTimePicker2);
+            Controls.Add(button1);
+            Controls.Add(label1);
+            Controls.Add(dateTimePicker1);
+            Controls.Add(textBox1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private DateTimePicker dateTimePicker1;
+        private Label label1;
+        private Button button1;
+        private DateTimePicker dateTimePicker2;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Button button2;
+        private ComboBox comboBox1;
+        private Label label6;
+    }
+}
---
+++ krediAPP/krediAPP/Form1.cs
@@ -0,0 +1,99 @@
+namespace krediAPP
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DateTime baslangýc = dateTimePicker1.Value;
+            DateTime bitis = dateTimePicker2.Value;
+
+
+            TimeSpan fark = bitis - baslangýc;
+            int günFarký = (int)fark.TotalDays;
+
+            label1.Text = "Gün Farký: " + günFarký.ToString();
+
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        { //eðer date timerlar kullanýlmak istenirse bu þekilde kullanýlabilir
+
+            /*  DateTime baslangýc = dateTimePicker1.Value;
+              DateTime bitis = dateTimePicker2.Value;
+
+
+              TimeSpan fark = bitis - baslangýc;
+              int günFarký = (int)fark.TotalDays;
+
+              if (günFarký < 30)
+              {
+                  label1.Text = "vade süresi en az 1 ay olmalýdýr";
+
+              }
+              else
+              { }
+            */
+
+            double anapara = Convert.ToDouble(textBox1.Text);
+            double faizOrani = Convert.ToDouble(textBox2.Text) / 100;
+            int vadeSuresi = Convert.ToInt32(textBox3.Text);
+
+
+            double aylikFaizOrani = faizOrani / 12;
+            double aylikOdeme = (anapara * aylikFaizOrani) / (1 - Math.Pow(1 + aylikFaizOrani, -vadeSuresi));
+            double toplamOdeme = aylikOdeme * vadeSuresi;
+            double toplamFaiz = toplamOdeme - anapara;
+            
+
+
+            label5.Text = "Aylýk Taksit: " + aylikOdeme.ToString("C");
+            label5.Text += "\nAnapara Miktarý:" + anapara.ToString();
+            label5.Text += "\nToplam Ödeme: " + toplamOdeme.ToString("C");
+            label5.Text += "\nToplam Faiz: " + toplamFaiz.ToString("C");
+            label5.Text += "\nTaksit Sayýsý: " + vadeSuresi.ToString();
+           
+
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
